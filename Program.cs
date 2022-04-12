5dd60e45308c13e2a2c53d5556c820776f7b79de
Emre Can Sari <cansarrr2@gmail.com>
2022-04-12 22:06:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace overloading__out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen integer'a dönüştürmek istediğiniz şeyi giriniz");
+            bool sonuc = int.TryParse(Console.ReadLine(), out int read);
+            if (sonuc)
+            {
+                Console.Write("başarılı!, değeriniz:"+ " " + read);
+                
+            }
+            else 
+                Console.WriteLine("başarısız");
+        
+            }
+
+        }
+    }
